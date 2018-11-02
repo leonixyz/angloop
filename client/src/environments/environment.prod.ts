dc78766b18c7fc8b52d2b1ad9a14cdcd72e1a050
leonixyz <leonixyz@gmail.com>
2018-11-02 20:43:04 +0100

    add navbar

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  API: 'http://192.168.1.106:3000/api'
+  API: 'http://192.168.1.106:3000/api',
+  AppName: 'My App'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  API: 'http://localhost:3000/api'
+  API: 'http://localhost:3000/api',
+  AppName: 'My App'
 };
 
 /*
